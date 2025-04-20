88c81aa8c3af6476f96a2f180a92e57af1cc2038
Arys <arys.steam@gmail.com>
2025-04-20 02:58:21 +0100

    Cleaned up FireSupport classes

--- project/SamSWAT.FireSupport/Unity/FireSupportController.cs
+++ project/SamSWAT.FireSupport/Unity/FireSupportController.cs
@@ -7,145 +7,137 @@ using System.Collections;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace SamSWAT.FireSupport.ArysReloaded.Unity
+namespace SamSWAT.FireSupport.ArysReloaded.Unity;
+
+public class FireSupportController : UIInputNode
 {
-    public class FireSupportController : UIInputNode
-    {
-        private bool _requestAvailable = true;
-        private Coroutine _timerCoroutine;
-        private FireSupportAudio _audio;
-        private FireSupportUI _ui;
-        private FireSupportSpotter _spotter;
-        private GesturesMenu _gesturesMenu;
+	private bool _requestAvailable = true;
+	private Coroutine _timerCoroutine;
+	private FireSupportAudio _audio;
+	private FireSupportUI _ui;
+	private FireSupportSpotter _spotter;
+	private GesturesMenu _gesturesMenu;
+	
+	public static FireSupportController Instance { get; private set; }
+	public bool StrafeRequestAvailable => _requestAvailable && AvailableStrafeRequests > 0;
+	public bool ExtractRequestAvailable => _requestAvailable && AvailableExtractRequests > 0;
+	public bool AnyRequestAvailable => _requestAvailable && (AvailableStrafeRequests > 0 || AvailableExtractRequests > 0);
+	public int AvailableStrafeRequests { get; private set; }
+	public int AvailableExtractRequests { get; private set; }
+	
+	public static async Task<FireSupportController> Init(GesturesMenu gesturesMenu)
+	{
+		Instance = new GameObject("FireSupportController").AddComponent<FireSupportController>();
+		Instance._audio = await FireSupportAudio.Load();
+		Instance._spotter = await FireSupportSpotter.Load();
+		Instance._ui = await FireSupportUI.Load(gesturesMenu);
+		Instance._gesturesMenu = gesturesMenu;
+		await FireSupportPool.LoadBundlesAndCreatePools();
+		//VehicleWeapon.Init();
+		Instance.AvailableStrafeRequests = FireSupportPlugin.AmountOfStrafeRequests.Value;
+		Instance.AvailableExtractRequests = FireSupportPlugin.AmountOfExtractionRequests.Value;
+		Instance._audio.PlayVoiceover(EVoiceoverType.StationReminder);
+		Instance._ui.SupportRequested += Instance.OnSupportRequested;
+		return Instance;
+	}
+	
+	private void OnDestroy()
+	{
+		AssetLoader.UnloadAllBundles();
+		_ui.SupportRequested -= OnSupportRequested;
+		StaticManager.KillCoroutine(ref _timerCoroutine);
+	}
+	
+	private void OnSupportRequested(ESupportType supportType)
+	{
+		switch (supportType)
+		{
+			case ESupportType.Strafe:
+				if (StrafeRequestAvailable)
+				{
+					_gesturesMenu.Close();
+					StaticManager.BeginCoroutine(_spotter.SpotterSequence(ESupportType.Strafe, (b, startPos, endPos) =>
+					{
+						if (!b)
+							StaticManager.BeginCoroutine(StrafeRequest(startPos, endPos));
+					}));
+				}
+				break;
+			case ESupportType.Extract:
+				if (ExtractRequestAvailable)
+				{
+					_gesturesMenu.Close();
+					StaticManager.BeginCoroutine(_spotter.SpotterSequence(ESupportType.Extract, (b, pos, rot) =>
+					{
+						if (!b)
+							StaticManager.BeginCoroutine(ExtractionRequest(pos, rot));
+					}));
+				}
+				break;
+		}
+	}
+	
+	private IEnumerator StrafeRequest(Vector3 strafeStartPos, Vector3 strafeEndPos)
+	{
+		IFireSupportOption a10 = FireSupportPool.TakeFromPool(ESupportType.Strafe);
+		Vector3 pos = (strafeStartPos + strafeEndPos) / 2;
+		Vector3 dir = (strafeEndPos - strafeStartPos).normalized;
+		AvailableStrafeRequests--;
+		_timerCoroutine = StaticManager.BeginCoroutine(Timer(FireSupportPlugin.RequestCooldown.Value));
+		_audio.PlayVoiceover(EVoiceoverType.StationStrafeRequest);
+		yield return new WaitForSeconds(8f);
+		_audio.PlayVoiceover(EVoiceoverType.JetArriving);
+		a10.ProcessRequest(pos, dir, Vector3.zero);
+	}
+	
+	private IEnumerator ExtractionRequest(Vector3 position, Vector3 rotation)
+	{
+		IFireSupportOption uh60 = FireSupportPool.TakeFromPool(ESupportType.Extract);
+		AvailableExtractRequests--;
+		_requestAvailable = false;
+		_audio.PlayVoiceover(EVoiceoverType.StationExtractionRequest);
+		yield return new WaitForSeconds(8f);
+		uh60.ProcessRequest(position, Vector3.zero, rotation);
+		_audio.PlayVoiceover(EVoiceoverType.SupportHeliArrivingToPickup);
+		yield return new WaitForSeconds(35f + FireSupportPlugin.HelicopterWaitTime.Value);
+		if (Instance == null) yield break;
+		_timerCoroutine = StaticManager.BeginCoroutine(Timer(FireSupportPlugin.RequestCooldown.Value));
+	}
+	
+	private IEnumerator Timer(float time)
+	{
+		_requestAvailable = false;
+		_ui.timerText.enabled = true;
+		while (time > 0)
+		{
+			time -= Time.deltaTime;
+			if (time < 0)
+			{
+				time = 0;
+			}
 
-        public static FireSupportController Instance { get; private set; }
-        public bool StrafeRequestAvailable => _requestAvailable && AvailableStrafeRequests > 0;
-        public bool ExtractRequestAvailable => _requestAvailable && AvailableExtractRequests > 0;
-        public bool AnyRequestAvailable => _requestAvailable && (AvailableStrafeRequests > 0 || AvailableExtractRequests > 0);
-        public int AvailableStrafeRequests { get; private set; }
-        public int AvailableExtractRequests { get; private set; }
-
-        public static async Task<FireSupportController> Init(GesturesMenu gesturesMenu)
-        {
-            Instance = new GameObject("FireSupportController").AddComponent<FireSupportController>();
-            Instance._audio = await FireSupportAudio.Load();
-            Instance._spotter = await FireSupportSpotter.Load();
-            Instance._ui = await FireSupportUI.Load(gesturesMenu);
-            Instance._gesturesMenu = gesturesMenu;
-            await FireSupportPool.LoadBundlesAndCreatePools();
-            WeaponClass.Init();
-            Instance.AvailableStrafeRequests = FireSupportPlugin.AmountOfStrafeRequests.Value;
-            Instance.AvailableExtractRequests = FireSupportPlugin.AmountOfExtractionRequests.Value;
-            Instance._audio.PlayVoiceover(EVoiceoverType.StationReminder);
-            Instance._ui.SupportRequested += Instance.OnSupportRequested;
-            return Instance;
-        }
-
-        private void OnSupportRequested(ESupportType supportType)
-        {
-            switch (supportType)
-            {
-                case ESupportType.Strafe:
-                    if (StrafeRequestAvailable)
-                    {
-                        _gesturesMenu.Close();
-                        StaticManager.BeginCoroutine(_spotter.SpotterSequence(ESupportType.Strafe, (b, startPos, endPos) =>
-                        {
-                            if (!b)
-                                StaticManager.BeginCoroutine(StrafeRequest(startPos, endPos));
-                        }));
-                    }
-                    break;
-                case ESupportType.Extract:
-                    if (ExtractRequestAvailable)
-                    {
-                        _gesturesMenu.Close();
-                        StaticManager.BeginCoroutine(_spotter.SpotterSequence(ESupportType.Extract, (b, pos, rot) =>
-                        {
-                            if (!b)
-                                StaticManager.BeginCoroutine(ExtractionRequest(pos, rot));
-                        }));
-                    }
-                    break;
-            }
-        }
-
-        private IEnumerator StrafeRequest(Vector3 strafeStartPos, Vector3 strafeEndPos)
-        {
-            var a10 = FireSupportPool.TakeFromPool(ESupportType.Strafe);
-            var pos = (strafeStartPos + strafeEndPos) / 2;
-            var dir = (strafeEndPos - strafeStartPos).normalized;
-            AvailableStrafeRequests--;
-            _timerCoroutine = StaticManager.BeginCoroutine(Timer(FireSupportPlugin.RequestCooldown.Value));
-            _audio.PlayVoiceover(EVoiceoverType.StationStrafeRequest);
-            yield return new WaitForSeconds(8f);
-            _audio.PlayVoiceover(EVoiceoverType.JetArriving);
-            a10.ProcessRequest(pos, dir, Vector3.zero);
-        }
-
-        private IEnumerator ExtractionRequest(Vector3 position, Vector3 rotation)
-        {
-            var uh60 = FireSupportPool.TakeFromPool(ESupportType.Extract);
-            AvailableExtractRequests--;
-            _requestAvailable = false;
-            _audio.PlayVoiceover(EVoiceoverType.StationExtractionRequest);
-            yield return new WaitForSeconds(8f);
-            uh60.ProcessRequest(position, Vector3.zero, rotation);
-            _audio.PlayVoiceover(EVoiceoverType.SupportHeliArrivingToPickup);
-            yield return new WaitForSeconds(35f + FireSupportPlugin.HelicopterWaitTime.Value);
-            if (Instance == null) yield break;
-            _timerCoroutine = StaticManager.BeginCoroutine(Timer(FireSupportPlugin.RequestCooldown.Value));
-        }
-
-        private IEnumerator Timer(float time)
-        {
-            _requestAvailable = false;
-            _ui.timerText.enabled = true;
-            while (time > 0)
-            {
-                time -= Time.deltaTime;
-                if (time < 0)
-                {
-                    time = 0;
-                }
-
-                float minutes = Mathf.FloorToInt(time / 60);
-                float seconds = Mathf.FloorToInt(time % 60);
-                _ui.timerText.text = $"{minutes:00}.{seconds:00}";
-                yield return null;
-            }
-            _ui.timerText.enabled = false;
-            _requestAvailable = true;
-            if (AnyRequestAvailable)
-            {
-                FireSupportAudio.Instance.PlayVoiceover(EVoiceoverType.StationAvailable);
-            }
-        }
-
-        public override ETranslateResult TranslateCommand(ECommand command)
-        {
-            if (command == ECommand.MumbleToggle)
-            {
-                Debug.LogError("MUMBLE");
-            }
-
-            return ETranslateResult.Ignore;
-        }
-
-        public override void TranslateAxes(ref float[] axes)
-        {
-        }
-
-        public override ECursorResult ShouldLockCursor()
-        {
-            return ECursorResult.Ignore;
-        }
-
-        private void OnDestroy()
-        {
-            AssetLoader.UnloadAllBundles();
-            _ui.SupportRequested -= OnSupportRequested;
-            StaticManager.KillCoroutine(ref _timerCoroutine);
-        }
-    }
+			float minutes = Mathf.FloorToInt(time / 60);
+			float seconds = Mathf.FloorToInt(time % 60);
+			_ui.timerText.text = $"{minutes:00}.{seconds:00}";
+			yield return null;
+		}
+		_ui.timerText.enabled = false;
+		_requestAvailable = true;
+		if (AnyRequestAvailable)
+		{
+			FireSupportAudio.Instance.PlayVoiceover(EVoiceoverType.StationAvailable);
+		}
+	}
+	
+	public override ETranslateResult TranslateCommand(ECommand command)
+	{
+		return ETranslateResult.Ignore;
+	}
+	
+	public override void TranslateAxes(ref float[] axes) {}
+	
+	public override ECursorResult ShouldLockCursor()
+	{
+		return ECursorResult.Ignore;
+	}
 }
--- project/SamSWAT.FireSupport/Unity/UI/FireSupportUI.cs
+++ project/SamSWAT.FireSupport/Unity/UI/FireSupportUI.cs
@@ -9,164 +9,178 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-namespace SamSWAT.FireSupport.ArysReloaded.Unity
+namespace SamSWAT.FireSupport.ArysReloaded.Unity;
+
+public class FireSupportUI : ComponentBase, IPointerEnterHandler, IPointerExitHandler
 {
-    public class FireSupportUI : MonoBehaviour, IComponent, IPointerEnterHandler, IPointerExitHandler
-    {
-        public GameObject SpotterNotice;
-        public GameObject SpotterHeliNotice;
-        public Text timerText;
-        [SerializeField] private FireSupportUIElement[] supportOptions;
-        [SerializeField] private HoverTooltipArea tooltip;
-        private Player _player;
-        private ESupportType _selectedSupportOption;
-        private float _menuOffset;
-
-        public static FireSupportUI Instance { get; private set; }
-        public bool IsUnderPointer { get; set; }
-
-        public event Action<ESupportType> SupportRequested;
-
-        public static async Task<FireSupportUI> Load(GesturesMenu gesturesMenu)
-        {
-            Instance = Instantiate(await AssetLoader.LoadAssetAsync("assets/content/ui/firesupport_ui.bundle")).GetComponent<FireSupportUI>();
-            Instance._player = Singleton<GameWorld>.Instance.MainPlayer;
-
-            var fireSupportUiT = Instance.transform;
-            fireSupportUiT.parent = gesturesMenu.transform;
-            fireSupportUiT.localPosition = new Vector3(0, -255, 0);
-            fireSupportUiT.localScale = new Vector3(1.4f, 1.4f, 1);
-            Instance._menuOffset = Screen.height / 2f - fireSupportUiT.position.y;
-
-            var infoPanelTransform = Instance.SpotterNotice.transform.parent;
-            infoPanelTransform.parent = Singleton<GameUI>.Instance.transform;
-            infoPanelTransform.localPosition = new Vector3(0, -370f, 0);
-            infoPanelTransform.localScale = Vector3.one;
-
-            return Instance;
-        }
-
-        public void ManualUpdate()
-        {
-            if (!gameObject.activeInHierarchy) return;
-            RenderUI();
-            HandleInput();
-        }
-
-        private void Start()
-        {
-            FireSupportPlugin.RegisterComponent(this);
-        }
-
-        private void OnDestroy()
-        {
-            FireSupportPlugin.DeregisterComponent(this);
-        }
-
-        private void RenderUI()
-        {
-            var enabledColor = new Color(1, 1, 1, 1);
-            var disabledColor = new Color(1, 1, 1, 0.4f);
-            var rangefinderInHands = _player.HandsController.Item.TemplateId == ItemConstants.RANGEFINDER_TPL;
-
-            tooltip.SetUnlockStatus(rangefinderInHands);
-
-            if (!rangefinderInHands)
-            {
-                return;
-            }
-
-            var fireSupportController = FireSupportController.Instance;
-            if (fireSupportController.StrafeRequestAvailable)
-            {
-                supportOptions[2].AmountText.color = enabledColor;
-                supportOptions[2].Icon.color = enabledColor;
-            }
-            else
-            {
-                supportOptions[2].IsUnderPointer = false;
-                supportOptions[2].AmountText.color = disabledColor;
-                supportOptions[2].Icon.color = disabledColor;
-            }
-
-            if (fireSupportController.ExtractRequestAvailable)
-            {
-                supportOptions[4].AmountText.color = enabledColor;
-                supportOptions[4].Icon.color = enabledColor;
-            }
-            else
-            {
-                supportOptions[4].IsUnderPointer = false;
-                supportOptions[4].AmountText.color = disabledColor;
-                supportOptions[4].Icon.color = disabledColor;
-            }
-
-            supportOptions[2].AmountText.text = fireSupportController.AvailableStrafeRequests.ToString();
-            supportOptions[4].AmountText.text = fireSupportController.AvailableExtractRequests.ToString();
-        }
-
-        private void HandleInput()
-        {
-            if (!IsUnderPointer) return;
-            var rangefinderInHands = _player.HandsController.Item.TemplateId == ItemConstants.RANGEFINDER_TPL;
-            if (!rangefinderInHands) return;
-            float angle = CalculateAngle();
-
-            for (int i = 0; i < supportOptions.Length; i++)
-            {
-                if (angle > i * 45 && angle < (i + 1) * 45 && FireSupportController.Instance.AnyRequestAvailable)
-                {
-                    supportOptions[i].IsUnderPointer = true;
-                    _selectedSupportOption = (ESupportType)i;
-                }
-                else
-                {
-                    supportOptions[i].IsUnderPointer = false;
-                }
-            }
-
-            if (!Input.GetMouseButtonDown(0)) return;
-            SupportRequested?.Invoke(_selectedSupportOption);
-        }
-
-        private float CalculateAngle()
-        {
-            Vector2 mouse;
-            mouse.x = Input.mousePosition.x - (Screen.width / 2f);
-            mouse.y = Input.mousePosition.y - (Screen.height / 2f) + _menuOffset;
-            mouse.Normalize();
-
-            if (mouse == Vector2.zero)
-            {
-                return 0;
-            }
-
-            float angle = Mathf.Atan2(mouse.y, -mouse.x) / Mathf.PI;
-            angle *= 180;
-            angle += 111;
-
-            if (angle < 0)
-            {
-                angle += 360;
-            }
-
-            return angle;
-        }
-
-        void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
-        {
-            IsUnderPointer = true;
-        }
-
-        void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
-        {
-            IsUnderPointer = false;
-
-            foreach (var t in supportOptions)
-            {
-                t.IsUnderPointer = false;
-            }
-        }
-    }
-
+	public GameObject SpotterNotice;
+	public GameObject SpotterHeliNotice;
+	public Text timerText;
+	[SerializeField] private FireSupportUIElement[] supportOptions;
+	[SerializeField] private HoverTooltipArea tooltip;
+	private Player _player;
+	private ESupportType _selectedSupportOption;
+	private float _menuOffset;
+	
+	public static FireSupportUI Instance { get; private set; }
+	public bool IsUnderPointer { get; set; }
+	
+	public event Action<ESupportType> SupportRequested;
+	
+	public static async Task<FireSupportUI> Load(GesturesMenu gesturesMenu)
+	{
+		Instance = Instantiate(await AssetLoader.LoadAssetAsync("assets/content/ui/firesupport_ui.bundle")).GetComponent<FireSupportUI>();
+		Instance._player = Singleton<GameWorld>.Instance.MainPlayer;
+		
+		Transform fireSupportUiT = Instance.transform;
+		fireSupportUiT.parent = gesturesMenu.transform;
+		fireSupportUiT.localPosition = new Vector3(0, -255, 0);
+		fireSupportUiT.localScale = new Vector3(1.4f, 1.4f, 1);
+		Instance._menuOffset = Screen.height / 2f - fireSupportUiT.position.y;
+		
+		Transform infoPanelTransform = Instance.SpotterNotice.transform.parent;
+		infoPanelTransform.parent = Singleton<GameUI>.Instance.transform;
+		infoPanelTransform.localPosition = new Vector3(0, -370f, 0);
+		infoPanelTransform.localScale = Vector3.one;
+		
+		return Instance;
+	}
+	
+	public override void ManualUpdate()
+	{
+		if (!gameObject.activeInHierarchy)
+		{
+			return;
+		}
+		
+		RenderUI();
+		HandleInput();
+	}
+	
+	private void OnEnable()
+	{
+		FireSupportPlugin.RegisterComponent(this);
+	}
+	
+	private void OnDisable()
+	{
+		FireSupportPlugin.DeregisterComponent(this);
+	}
+	
+	private void RenderUI()
+	{
+		var enabledColor = new Color(1, 1, 1, 1);
+		var disabledColor = new Color(1, 1, 1, 0.4f);
+		bool rangefinderInHands = _player.HandsController.Item.TemplateId == ItemConstants.RANGEFINDER_TPL;
+		
+		tooltip.SetUnlockStatus(rangefinderInHands);
+		
+		if (!rangefinderInHands)
+		{
+			return;
+		}
+		
+		var fireSupportController = FireSupportController.Instance;
+		if (fireSupportController.StrafeRequestAvailable)
+		{
+			supportOptions[2].AmountText.color = enabledColor;
+			supportOptions[2].Icon.color = enabledColor;
+		}
+		else
+		{
+			supportOptions[2].IsUnderPointer = false;
+			supportOptions[2].AmountText.color = disabledColor;
+			supportOptions[2].Icon.color = disabledColor;
+		}
+		
+		if (fireSupportController.ExtractRequestAvailable)
+		{
+			supportOptions[4].AmountText.color = enabledColor;
+			supportOptions[4].Icon.color = enabledColor;
+		}
+		else
+		{
+			supportOptions[4].IsUnderPointer = false;
+			supportOptions[4].AmountText.color = disabledColor;
+			supportOptions[4].Icon.color = disabledColor;
+		}
+		
+		supportOptions[2].AmountText.text = fireSupportController.AvailableStrafeRequests.ToString();
+		supportOptions[4].AmountText.text = fireSupportController.AvailableExtractRequests.ToString();
+	}
+	
+	private void HandleInput()
+	{
+		if (!IsUnderPointer)
+		{
+			return;
+		}
+		
+		bool rangefinderInHands = _player.HandsController.Item.TemplateId == ItemConstants.RANGEFINDER_TPL;
+		if (!rangefinderInHands)
+		{
+			return;
+		}
+		
+		float angle = CalculateAngle();
+		
+		for (int i = 0; i < supportOptions.Length; i++)
+		{
+			if (angle > i * 45 && angle < (i + 1) * 45 && FireSupportController.Instance.AnyRequestAvailable)
+			{
+				supportOptions[i].IsUnderPointer = true;
+				_selectedSupportOption = (ESupportType)i;
+			}
+			else
+			{
+				supportOptions[i].IsUnderPointer = false;
+			}
+		}
+		
+		if (!Input.GetMouseButtonDown(0))
+		{
+			return;
+		}
+		
+		SupportRequested?.Invoke(_selectedSupportOption);
+	}
+	
+	private float CalculateAngle()
+	{
+		Vector2 mouse;
+		mouse.x = Input.mousePosition.x - (Screen.width / 2f);
+		mouse.y = Input.mousePosition.y - (Screen.height / 2f) + _menuOffset;
+		mouse.Normalize();
+		
+		if (mouse == Vector2.zero)
+		{
+			return 0;
+		}
+		
+		float angle = Mathf.Atan2(mouse.y, -mouse.x) / Mathf.PI;
+		angle *= 180;
+		angle += 111;
+		
+		if (angle < 0)
+		{
+			angle += 360;
+		}
+		
+		return angle;
+	}
+	
+	void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+	{
+		IsUnderPointer = true;
+	}
+	
+	void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+	{
+		IsUnderPointer = false;
+		
+		foreach (FireSupportUIElement t in supportOptions)
+		{
+			t.IsUnderPointer = false;
+		}
+	}
 }
