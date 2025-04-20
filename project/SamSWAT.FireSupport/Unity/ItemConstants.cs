302b56d4a0549845f9f059f387c16900182014c4
Arys <arys.steam@gmail.com>
2025-04-20 02:40:42 +0100

    Cleaned up ItemConstants.cs

--- project/SamSWAT.FireSupport/Unity/ItemConstants.cs
+++ project/SamSWAT.FireSupport/Unity/ItemConstants.cs
@@ -1,10 +1,9 @@
-﻿namespace SamSWAT.FireSupport.ArysReloaded.Unity
+﻿namespace SamSWAT.FireSupport.ArysReloaded.Unity;
+
+public static class ItemConstants
 {
-    public static class ItemConstants
-    {
-        public const string
-            RANGEFINDER_TPL = "61605e13ffa6e502ac5e7eef",
-            GAU8_AMMO_TPL = "660b2d05cec10101410e7d7a",
-            GAU8_WEAPON_TPL = "660b2d05cec10101410e7d7b";
-    }
+	public const string
+		RANGEFINDER_TPL = "61605e13ffa6e502ac5e7eef",
+		GAU8_AMMO_TPL = "660b2d05cec10101410e7d7a",
+		GAU8_WEAPON_TPL = "660b2d05cec10101410e7d7b";
 }
