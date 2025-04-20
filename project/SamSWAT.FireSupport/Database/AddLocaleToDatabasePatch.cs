ed5c83fe214fd0ac77389019509673fc675725de
Arys <arys.steam@gmail.com>
2025-04-20 02:37:52 +0100

    Refactored AddLocaleToDatabasePatch.cs

--- project/SamSWAT.FireSupport/Database/AddLocaleToDatabasePatch.cs
+++ project/SamSWAT.FireSupport/Database/AddLocaleToDatabasePatch.cs
@@ -1,37 +1,27 @@
-﻿using SamSWAT.FireSupport.ArysReloaded.Unity;
+﻿using HarmonyLib;
+using SamSWAT.FireSupport.ArysReloaded.Unity;
 using SPT.Reflection.Patching;
-using SPT.Reflection.Utils;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
-namespace SamSWAT.FireSupport.ArysReloaded.Database
+namespace SamSWAT.FireSupport.ArysReloaded.Database;
+
+public class AddLocaleToDatabasePatch : ModulePatch
 {
-	public class AddLocaleToDatabasePatch : ModulePatch
+	protected override MethodBase GetTargetMethod()
 	{
-		protected override MethodBase GetTargetMethod()
-		{
-			return PatchConstants.EftTypes.First(IsTargetType).GetMethod("GetLocalization");
-		}
-
-		private bool IsTargetType(Type t)
-		{
-			return typeof(IBackEndSession).IsAssignableFrom(t) && t.IsAbstract;
-		}
-
-		[PatchPostfix]
-		private static async void PatchPostfix(Task<Dictionary<string, string>> __result)
-		{
-			var locales = await __result;
-			locales.Add($"{ItemConstants.GAU8_AMMO_TPL} Name", "PGU-13/B HEI High Explosive Incendiary");
-			locales.Add($"{ItemConstants.GAU8_AMMO_TPL} ShortName", "PGU-13/B HEI");
-			locales.Add($"{ItemConstants.GAU8_AMMO_TPL} Description", "The PGU-13/B HEI High Explosive Incendiary round employs a standard M505 fuze and explosive mixture with a body of naturally fragmenting material that is effective against lighter vehicle and material targets.");
-
-			locales.Add($"{ItemConstants.GAU8_WEAPON_TPL} Name", "Fairchild Republic A-10 Thunderbolt II");
-			locales.Add($"{ItemConstants.GAU8_WEAPON_TPL} ShortName", "A-10 Thunderbolt II");
-			locales.Add($"{ItemConstants.GAU8_WEAPON_TPL} Description", "Close air support attack aircraft developed by Fairchild Republic for the USAF with mounted GAU-8/A Avenger 30mm autocannon.");
-		}
+		return AccessTools.Method(typeof(LocaleManagerClass), nameof(LocaleManagerClass.UpdateLocales));
+	}
+	
+	[PatchPostfix]
+	private static void PatchPostfix(Dictionary<string, string> newLocale)
+	{
+		newLocale.TryAdd($"{ItemConstants.GAU8_AMMO_TPL} Name", "PGU-13/B HEI High Explosive Incendiary");
+		newLocale.TryAdd($"{ItemConstants.GAU8_AMMO_TPL} ShortName", "PGU-13/B HEI");
+		newLocale.TryAdd($"{ItemConstants.GAU8_AMMO_TPL} Description", "The PGU-13/B HEI High Explosive Incendiary round employs a standard M505 fuze and explosive mixture with a body of naturally fragmenting material that is effective against lighter vehicle and material targets.");
+		
+		newLocale.TryAdd($"{ItemConstants.GAU8_WEAPON_TPL} Name", "Fairchild Republic A-10 Thunderbolt II");
+		newLocale.TryAdd($"{ItemConstants.GAU8_WEAPON_TPL} ShortName", "A-10 Thunderbolt II");
+		newLocale.TryAdd($"{ItemConstants.GAU8_WEAPON_TPL} Description", "Close air support attack aircraft developed by Fairchild Republic for the USAF with mounted GAU-8/A Avenger 30mm autocannon.");
 	}
 }
