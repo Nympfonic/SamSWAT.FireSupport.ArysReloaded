06551cd43350a2f016193563f308fb0f4f392491
Arys <arys.steam@gmail.com>
2025-04-20 02:39:20 +0100

    Refactored ColliderReporter.cs

--- project/SamSWAT.FireSupport/Unity/ColliderReporter.cs
+++ project/SamSWAT.FireSupport/Unity/ColliderReporter.cs
@@ -1,54 +1,57 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace SamSWAT.FireSupport.ArysReloaded.Unity
+namespace SamSWAT.FireSupport.ArysReloaded.Unity;
+
+public class ColliderReporter : ComponentBase
 {
-    public class ColliderReporter : MonoBehaviour, IComponent
-    {
-        [NonSerialized] 
-        public bool HasCollision;
-        private BoxCollider[] _colliders;
-        private Collider[] _intersectedColliders;
-        private int _mask;
-
-        public void ManualUpdate()
-        {
-            foreach (var col in _colliders)
-            {
-                var colTransform = col.transform;
-                var colRotation = colTransform.rotation;
-                var center = colTransform.position + colRotation * col.center;
-                var extents = Vector3.Scale(col.size * 0.5f, colTransform.lossyScale);
-                
-                var hits = Physics.OverlapBoxNonAlloc(
-                    center,
-                    extents,
-                    _intersectedColliders,
-                    colRotation,
-                    _mask,
-                    QueryTriggerInteraction.Ignore);
-
-                if (hits > 0)
-                {
-                    HasCollision = true;
-                    break;
-                }
-
-                HasCollision = false;
-            }
-        }
-
-        private void Start()
-        {
-            FireSupportPlugin.RegisterComponent(this);
-            _intersectedColliders = new Collider[5];
-            _colliders = GetComponents<BoxCollider>();
-            _mask = LayerMask.GetMask("LowPolyCollider", "HighPolyCollider");
-        }
-
-        private void OnDestroy()
-        {
-            FireSupportPlugin.DeregisterComponent(this);
-        }
-    }
+	private bool _hasCollision;
+	private BoxCollider[] _colliders;
+	private Collider[] _intersectedColliders;
+	private int _mask;
+	
+	public bool HasCollision => _hasCollision;
+	
+	public override void ManualUpdate()
+	{
+		foreach (BoxCollider col in _colliders)
+		{
+			Transform colTransform = col.transform;
+			Quaternion colRotation = colTransform.rotation;
+			Vector3 center = colTransform.position + colRotation * col.center;
+			Vector3 extents = Vector3.Scale(col.size * 0.5f, colTransform.lossyScale);
+			
+			int hits = Physics.OverlapBoxNonAlloc(
+				center,
+				extents,
+				_intersectedColliders,
+				colRotation,
+				_mask,
+				QueryTriggerInteraction.Ignore);
+			
+			if (hits > 0)
+			{
+				_hasCollision = true;
+				break;
+			}
+			
+			_hasCollision = false;
+		}
+	}
+	
+	private void Start()
+	{
+		_intersectedColliders = new Collider[5];
+		_colliders = GetComponents<BoxCollider>();
+		_mask = LayerMask.GetMask("LowPolyCollider", "HighPolyCollider");
+	}
+	
+	private void OnEnable()
+	{
+		FireSupportPlugin.RegisterComponent(this);
+	}
+	
+	private void OnDisable()
+	{
+		FireSupportPlugin.DeregisterComponent(this);
+	}
 }
