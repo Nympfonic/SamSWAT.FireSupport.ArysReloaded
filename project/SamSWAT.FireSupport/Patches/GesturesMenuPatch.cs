3fa7eccce8a5829c30f72b542d9a0985439ad549
Arys <arys.steam@gmail.com>
2025-04-20 02:41:38 +0100

    Cleaned up GesturesMenuPatch.cs

--- project/SamSWAT.FireSupport/Patches/GesturesMenuPatch.cs
+++ project/SamSWAT.FireSupport/Patches/GesturesMenuPatch.cs
@@ -2,6 +2,7 @@
 using EFT;
 using EFT.Airdrop;
 using EFT.InputSystem;
+using EFT.InventoryLogic;
 using EFT.UI.Gestures;
 using HarmonyLib;
 using SamSWAT.FireSupport.ArysReloaded.Unity;
@@ -11,56 +12,56 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
-namespace SamSWAT.FireSupport.ArysReloaded.Patches
+namespace SamSWAT.FireSupport.ArysReloaded.Patches;
+
+public class GesturesMenuPatch : ModulePatch
 {
-	public class GesturesMenuPatch : ModulePatch
+	protected override MethodBase GetTargetMethod()
 	{
-		protected override MethodBase GetTargetMethod()
+		return typeof(GesturesMenu).GetMethod(nameof(GesturesMenu.Init));
+	}
+	
+	[PatchPostfix]
+	public static async void PatchPostfix(GesturesMenu __instance)
+	{
+		if (!IsFireSupportAvailable())
 		{
-			return typeof(GesturesMenu).GetMethod(nameof(GesturesMenu.Init));
+			return;
 		}
-
-		[PatchPostfix]
-		public static async void PatchPostfix(GesturesMenu __instance)
+		
+		var owner = Singleton<GameWorld>.Instance.MainPlayer.GetComponent<GamePlayerOwner>();
+		FireSupportController fireSupportController = await FireSupportController.Init(__instance);
+		Traverse.Create(owner).Field<List<InputNode>>("_children").Value.Add(fireSupportController);
+		
+		var gesturesBindPanel = __instance.gameObject.GetComponentInChildren<GesturesBindPanel>(includeInactive: true);
+		gesturesBindPanel.transform.localPosition = new Vector3(0, -530, 0);
+	}
+	
+	private static bool IsFireSupportAvailable()
+	{
+		GameWorld gameWorld = Singleton<GameWorld>.Instance;
+		if (gameWorld == null)
 		{
-			if (!IsFireSupportAvailable())
-			{
-				return;
-			}
-
-			var owner = Singleton<GameWorld>.Instance.MainPlayer.GetComponent<GamePlayerOwner>();
-			var fireSupportController = await FireSupportController.Init(__instance);
-			Traverse.Create(owner).Field<List<InputNode>>("_children").Value.Add(fireSupportController);
-			var gesturesBindPanel = __instance.gameObject.GetComponentInChildren<GesturesBindPanel>(true);
-			gesturesBindPanel.transform.localPosition = new Vector3(0, -530, 0);
+			return false;
 		}
-
-		private static bool IsFireSupportAvailable()
+		
+		bool locationIsSuitable = gameWorld.MainPlayer.Location.ToLower() == "sandbox"
+			|| LocationScene.GetAll<AirdropPoint>().Any();
+		
+		if (!FireSupportPlugin.Enabled.Value || FireSupportController.Instance != null || !locationIsSuitable)
 		{
-			var gameWorld = Singleton<GameWorld>.Instance;
-			if (gameWorld == null)
-			{
-				return false;
-			}
-
-			var locationIsSuitable = gameWorld.MainPlayer.Location.ToLower() == "sandbox"
-				|| LocationScene.GetAll<AirdropPoint>().Any();
-
-			if (!FireSupportPlugin.Enabled.Value || FireSupportController.Instance != null || !locationIsSuitable)
-			{
-				return false;
-			}
-
-			var player = gameWorld.RegisteredPlayers[0];
-			if (!(player is LocalPlayer))
-			{
-				return false;
-			}
-
-			var inventory = player.Profile.Inventory;
-			var hasRangefinder = inventory.AllRealPlayerItems.Any(x => x.TemplateId == ItemConstants.RANGEFINDER_TPL);
-
-			return hasRangefinder;
+			return false;
 		}
+		
+		Player player = gameWorld.MainPlayer;
+		if (player == null)
+		{
+			return false;
+		}
+		
+		Inventory inventory = player.Profile.Inventory;
+		bool hasRangefinder = inventory.AllRealPlayerItems.Any(x => x.TemplateId == ItemConstants.RANGEFINDER_TPL);
+		
+		return hasRangefinder;
 	}
 }
